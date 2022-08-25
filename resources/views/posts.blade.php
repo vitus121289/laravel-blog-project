2412357359046a2bbfd28f58e514bc90cdcfa613
vitus121289 <vmtusing@gmail.com>
2022-08-25 14:58:56 +0800

    Included an if statement in posts.blade.php to check if the posts array is empty or not.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,14 +2,17 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if (empty($posts))
+            <x-post-featured :post="$posts[0]"></x-post-featured>
 
-        <x-post-featured :post="$posts[0]"></x-post-featured>
-
-        <div class="lg:grid lg:grid-cols-6">
-            @foreach ($posts->skip(1) as $post)
-                <x-posts-card :post="$post" class="{{ $loop->iteration > 2 ? 'col-span-2' : 'col-span-3' }}"></x-posts-card>
-            @endforeach
-        </div>
+            <div class="lg:grid lg:grid-cols-6">
+                @foreach ($posts->skip(1) as $post)
+                    <x-posts-card :post="$post" class="{{ $loop->iteration > 2 ? 'col-span-2' : 'col-span-3' }}"></x-posts-card>
+                @endforeach
+            </div>
+        @else
+            <p class="text-center">No posts yet. Check back later.</p>
+        @endif
     </main>
 
     @include('_posts-footer')

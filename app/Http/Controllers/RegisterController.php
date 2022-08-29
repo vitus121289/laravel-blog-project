3a333c8d29c0bfe17e81f978b00b06f24841b34c
Vincent <vmtusing@gmail.com>
2022-08-29 22:56:17 +0800

    Updated to so that upon successful registration of a user, that user will be automatically logged in.

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -14,12 +14,15 @@ class RegisterController extends Controller
     
     public function store() {
 
-        User::create(request()->validate([
+        $attributes = request()->validate([
             'name' => ['required', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email')],
             'username' => ['required', 'min:3', 'max:255', Rule::unique('users', 'username')],
             'password' => ['required', 'min:7', 'max:255']
-        ]));
+        ]);
+        $user = User::create($attributes);
+
+        auth()->login($user);
         
         return redirect('/')->with('success', 'Your account has account has been created');
     }

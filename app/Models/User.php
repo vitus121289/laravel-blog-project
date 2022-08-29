f89304377a098ac14cf36ea7511cf82a3d17b46e
vitus121289 <vmtusing@gmail.com>
2022-08-29 15:55:35 +0800

    Created a mutator for the password attribute to return a hashed version of the password.

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
     public function posts() {
         return $this->hasMany(Post::class);
     }
+
+    public function setPasswordAttribute($password) {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }

f715f708fa9dea0905ee5cca0b80f81fbd536a65
Vincent <vmtusing@gmail.com>
2022-08-30 11:20:11 +0800

    Created a blade component for post comments.

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,16 @@
+<article class="flex border border-gray-200 bg-gray-100 rounded-xl p-6 space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">Posted
+                    <time>
+                        8 months ago
+                    </time>
+                </p>
+        </header>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, temporibus ea cupiditate accusamus soluta sapiente voluptatibus at. Enim, voluptas sint vitae placeat rerum nesciunt sunt repellat maxime rem facere vero.</p>
+    </div>
+</article>

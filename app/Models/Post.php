7ec32f90eba8520eb98d5b2afec8692bb08d8e2d
Vincent <vmtusing@gmail.com>
2022-08-30 13:09:14 +0800

    Edited to create a relationship with comments.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -35,6 +35,10 @@ class Post extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
     public function scopeFilter($query, array $filters) {
         $query->when($filters['search'] ?? false, fn($query, $search) =>
             $query->where(fn($query) =>

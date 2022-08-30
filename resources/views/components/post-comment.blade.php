a88de1028319536388a7f2a69ca9f9d82ec81176
Vincent <vmtusing@gmail.com>
2022-08-30 13:05:52 +0800

    Modified to make comments dynamic.

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,16 +1,19 @@
+@props(['comment'])
 <article class="flex border border-gray-200 bg-gray-100 rounded-xl p-6 space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/?id={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->name }}</h3>
             <p class="text-xs">Posted
                     <time>
-                        8 months ago
+                        {{ $comment->created_at->diffForHumans() }}
                     </time>
                 </p>
         </header>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, temporibus ea cupiditate accusamus soluta sapiente voluptatibus at. Enim, voluptas sint vitae placeat rerum nesciunt sunt repellat maxime rem facere vero.</p>
+        <p>
+            {{ $comment->body }}
+        </p>
     </div>
 </article>

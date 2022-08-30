46306964c8129c1336115ddb64917a6c603c6c31
Vincent <vmtusing@gmail.com>
2022-08-30 10:41:50 +0800

    Removed the include directive for the footer and header as it is only relevant for pages that shows posts.

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,73 +1,71 @@
 <x-layout>
-    {{-- @include('posts/_header') --}}
-        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border-gray-100 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Register!</h1>
-            <form action="/register" method="post">
-                @csrf
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="name">
-                        Name
-                    </label>
+    <main class="max-w-lg mx-auto mt-10 bg-gray-100 border-gray-100 p-6 rounded-xl">
+        <h1 class="text-center font-bold text-xl">Register!</h1>
+        <form action="/register" method="post">
+            @csrf
+            <div class="mb-6">
+                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                    for="name">
+                    Name
+                </label>
+                <input type="text"
+                    class="border border-gray-400 p-2 w-full"
+                    name="name"
+                    id="name"
+                    value="{{ old('name') }}"
+                    required>
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+            </div>
+            <div class="mb-6">
+                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                    for="email">Email Address</label>
                     <input type="text"
                         class="border border-gray-400 p-2 w-full"
-                        name="name"
-                        id="name"
-                        value="{{ old('name') }}"
+                        name="email"
+                        id="email"
+                        value="{{ old('email') }}"
                         required>
-                        @error('name')
+                        @error('email')
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                         @enderror
-                </div>
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="email">Email Address</label>
-                        <input type="text"
-                            class="border border-gray-400 p-2 w-full"
-                            name="email"
-                            id="email"
-                            value="{{ old('email') }}"
-                            required>
-                            @error('email')
-                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                            @enderror
-                </div>
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="username">
-                        Username
-                    </label>
-                    <input type="text"
-                        class="border border-gray-400 p-2 w-full"
-                        name="username"
-                        id="username"
-                        value="{{ old('username') }}"
-                        required>
-                        @error('username')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                        @enderror
-                </div>
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="password">
-                        Password
-                    </label>
-                    <input type="password"
-                        class="border border-gray-400 p-2 w-full"
-                        name="password"
-                        id="password"
-                        required>
-                        @error('password')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                        @enderror
-                </div>
-                <div class="mb-6">
-                    <button type="submit"
-                        class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </main>
-    @include('posts/_footer')
+            </div>
+            <div class="mb-6">
+                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                    for="username">
+                    Username
+                </label>
+                <input type="text"
+                    class="border border-gray-400 p-2 w-full"
+                    name="username"
+                    id="username"
+                    value="{{ old('username') }}"
+                    required>
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+            </div>
+            <div class="mb-6">
+                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                    for="password">
+                    Password
+                </label>
+                <input type="password"
+                    class="border border-gray-400 p-2 w-full"
+                    name="password"
+                    id="password"
+                    required>
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+            </div>
+            <div class="mb-6">
+                <button type="submit"
+                    class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+                    Submit
+                </button>
+            </div>
+        </form>
+    </main>
 </x-layout>

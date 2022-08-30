b6ecf9d3c52053f292a11491dc9f4d84e59a5894
Vincent <vmtusing@gmail.com>
2022-08-30 10:43:52 +0800

    Created a method that handles a user login.

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
+    public function create() {
+        return view('sessions.create');
+    }
+
+    public function store() {
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            return redirect('/')->with('success', 'Welcome Back');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'We can\'t authenticate you.'
+        ]);
+    }
     public function destroy() {
         auth()->logout();
 

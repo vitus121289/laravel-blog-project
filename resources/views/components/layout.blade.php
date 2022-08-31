aa7cd23fb8e476882a8eb3afee473a9d1b2da18d
Vincent <vmtusing@gmail.com>
2022-08-31 10:24:03 +0800

    Edited to redirect to the footer when clicking the subscribe button on the nav bar.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,7 +26,7 @@
                     <a href="/register" class="text-xs font-bold uppercase">Register</a>
                     <a href="/login" class="text-xs font-bold uppercase ml-4">Login</a>
                 @endauth
-                <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+                <a href="#newsletter" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                     Subscribe for Updates
                 </a>
             </div>

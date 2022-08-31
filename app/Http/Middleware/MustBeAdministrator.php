a63a2b5882246e122125ced150a8f1d8b0d519aa
Vincent <vmtusing@gmail.com>
2022-08-31 12:55:37 +0800

    Created a middleware that checks if an authenticated user is an admin.

---
+++ app/Http/Middleware/MustBeAdministrator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Http\Request;
+
+class MustBeAdministrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->user()?->username != 'vtester') {
+            abort(403);
+        }
+        return $next($request);
+    }
+}

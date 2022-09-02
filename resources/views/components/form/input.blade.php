bc7835886deccacb7143aca4ac34c8b08dc4423a
Vincent <vmtusing@gmail.com>
2022-09-02 13:54:40 +0800

    Created components for the form.

---
+++ resources/views/components/form/input.blade.php
@@ -0,0 +1,13 @@
+@props(['name', 'type' => 'text'])
+<x-form.field>
+    <x-form.label name="{{ $name }}" />
+    <input
+        type="{{ $type }}"
+        name="{{ $name }}"
+        id="{{ $name }}"
+        class="border border-gray-200 p-2 w-full rounded"
+        {{-- required --}}
+        {{ $attributes(['value' => old($name)]) }}
+    >
+    <x-form.error name="{{ $name }}" />
+</x-form.field>

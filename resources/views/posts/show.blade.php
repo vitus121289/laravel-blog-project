45d11cca00e78ab69f1254ad64970bc5b7cbac50
Vincent <vmtusing@gmail.com>
2022-09-02 14:02:22 +0800

    Edited to show post thumbnails, and to use the category button component.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,7 @@
     <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
         <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
             <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
-                <img src="/images/illustration-1.png" alt="" class="rounded-xl">
+                <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="" class="rounded-xl">
 
                 <p class="mt-4 block text-gray-400 text-xs">
                     Published <time>{{ $post->created_at->diffForHumans() }}</time>
@@ -22,9 +22,9 @@
             </div>
 
             <div class="col-span-8">
-                <div class="hidden lg:flex justify-between mb-6">
+                <div class="lg:flex justify-between mb-6">
                     <a href="/"
-                        class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">
+                        class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500 py-4">
                         <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
                             <g fill="none" fill-rule="evenodd">
                                 <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
@@ -37,11 +37,7 @@
                         Back to Posts
                     </a>
 
-                    <div class="space-x-2">
-                        <a href="/categories/{{ $post->category->slug }}"
-                            class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                            style="font-size: 10px">{{ $post->category->name }}</a>
-                    </div>
+                    <x-category-button :category="$post->category" />
                 </div>
 
                 <h1 class="font-bold text-3xl lg:text-4xl mb-10">

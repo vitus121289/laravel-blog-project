1b374951ed538733248b48c5d6efdcbdc03cc650
Vincent <vmtusing@gmail.com>
2022-09-02 14:00:23 +0800

    Edited to use of the form components.

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,46 +1,15 @@
 <x-layout>
     <section class="px-6 py-8">
-        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Log In!</h1>
-            <form action="/login" method="post" class="mt-10">
-                @csrf
-                <div class="mb-6">
-                    <label 
-                        class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="email">
-                        E-mail
-                    </label>
-                    <input
-                        class="border border-gray-400 p-2 w-full"
-                        type="email"
-                        name="email"
-                        id="email"
-                        value="{{ old('email') }}"
-                        required>
-                        @error('email')
-                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                        @enderror
-                </div>
-                <div class="mb-6">
-                    <label
-                        class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="password">
-                        Password
-                    </label>
-                    <input
-                        class="border border-gray-400 p-2 w-full"
-                        type="password"
-                        name="password"
-                        id="password">
-                </div>
-                <div class="mb-6">
-                    <button
-                        class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
-                        type="submit">
-                            Log In
-                    </button>
-                </div>
-            </form>
+        <main class="max-w-lg mx-auto mt-10">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Log In!</h1>
+                <form action="/login" method="post" class="mt-10">
+                    @csrf
+                    <x-form.input name="email" type="email" />
+                    <x-form.input name="password" type="password" />
+                    <x-form.button>Log In</x-form.button>
+                </form>
+            </x-panel>
         </main>
     </section>
 </x-layout>

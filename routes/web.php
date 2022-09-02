015d6f668156b5c230f49e3ecfacf01e018c605a
Vincent <vmtusing@gmail.com>
2022-09-02 14:18:25 +0800

    Sorted the routes according to their controllers, and refactored the admin postcontroller routes so that all of it passes through the middleware that checks for admin privileges.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{NewsletterController, PostController, RegisterController, SessionController, PostCommentController};
+use App\Http\Controllers\{
+    NewsletterController,
+    PostController,
+    RegisterController,
+    SessionController,
+    PostCommentController};
+use App\Http\Controllers\Admin\{
+    PostController as AdminPostController};
 
 /*
 |--------------------------------------------------------------------------
@@ -14,19 +21,39 @@ use App\Http\Controllers\{NewsletterController, PostController, RegisterControll
 |
 */
 
-/*
-These are the routes for a GET request.
-*/
+/**
+ * PostController routes.
+ */
 Route::get('/', [PostController::class, 'index'])->name('home');
-Route::get('post/{post:slug}', [PostController::class, 'show']);
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::post('posts/{post:slug}/comments', [PostCommentController::class, 'store']);
+
+/**
+ * RegisterController routes.
+ */
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
-Route::get('login', [SessionController::class, 'create'])->middleware('guest');
-Route::get('posts/admin/create', [PostController::class, 'create'])->middleware('admin');
-/*
-These are the routes for a POST request.
-*/
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+
+/**
+ * SessionController routes.
+ */
+Route::get('login', [SessionController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
-Route::post('post/{post:slug}/comments', [PostCommentController::class, 'store']);
+
+/**
+ * AdminPostController routes.
+ */
+Route::middleware('can:admin')->group(function() {
+    Route::get('admin/posts/', [AdminPostController::class, 'index'])->name('adminDashboard');
+    Route::get('admin/posts/create', [AdminPostController::class, 'create'])->name('createPost');
+    Route::post('admin/posts/store', [AdminPostController::class, 'store']);
+    Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
+    Route::patch('admin/posts/{post}/', [AdminPostController::class, 'update']);
+    Route::delete('admin/posts/{post}/', [AdminPostController::class, 'destroy']);
+});
+
+/**
+ * NewsletterController routes.
+ */
 Route::post('newsletter', NewsletterController::class);     // This uses and invoked method.
